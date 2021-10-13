5b22d79208072eea5a35c08576354ce606060e32
filipi <filipisee@hotmail.com>
2021-10-13 13:50:04 -0300

    app.get method added

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,20 @@
 import express from "express";
 
+// @types/express
 const app = express();
+
+/*
+- GET    => Buscar uma informação
+- POST   => Inserir (criar) uma informação
+- PUT    => Alterar uma informação
+- DELETE => Remover dado(s)
+- PATCH  => Alterar uma informação específica
+*/
+
+app.get("/teste", (request, response) => {
+  // Request  => Entrando
+  // Response => Saindo
+});
 
 //http://localhost:3000
 app.listen(3000, () => console.log("Server is running! Massa de mais!"));
