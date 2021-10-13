e15db4a705ee62629c3f51780a35d18ae470331e
filipi <filipisee@hotmail.com>
2021-10-13 13:38:55 -0300

    updated server.js

---
+++ src/server.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = (0, express_1.default)();
+//http://localhost:3000
+app.listen(3000, function () { return console.log("Server is running! Massa de mais!"); });
